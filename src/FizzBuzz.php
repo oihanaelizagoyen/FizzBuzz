ac10470529f0ad369bf54ffa961cbf8f5d66904a
Oihana Elizagoyen Ugalde (portátil) <elizagoyen.131910@e.unavarra.es>
2022-02-21 23:10:35 +0100

    Se ha realizado el ejercicio de la Kata de FizzBuzz en el fichero FizzBuzz.php y también se ha realizado el test de dicho ejercicio en el fichero FizzBuzzTest.php

--- src/FizzBuzz.php
+++ src/FizzBuzz.php
@@ -4,5 +4,17 @@ namespace Deg540\PHPTestingBoilerplate;
 
 class FizzBuzz
 {
-
+    public function calculate(int $numberToBeConverted): string
+    {
+        if ($numberToBeConverted%15 == 0 || ((strpos(strval($numberToBeConverted), '3') !== false) && (strpos(strval($numberToBeConverted), '5') !== false))){
+            return 'fizzbuzz';
+        }
+        if ($numberToBeConverted%3 == 0 || strpos(strval($numberToBeConverted), '3') !== false){
+            return 'fizz';
+        }
+        if ($numberToBeConverted%5 == 0 || strpos(strval($numberToBeConverted), '5') !== false){
+            return 'buzz';
+        }
+        return strval($numberToBeConverted);
+    }
 }
