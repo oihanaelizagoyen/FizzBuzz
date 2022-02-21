d628d1582134fb52d7cbe8b3547064839d8e00e4
Oihana Elizagoyen Ugalde <elizagoyen.131910@e.unavarra.es>
2022-02-21 17:49:15 +0000

    Se ha corregido el fichero Calculator.php

--- src/Calculator.php
+++ src/Calculator.php
@@ -6,11 +6,11 @@ class Calculator
 {
     function add(int $number1, int $number2): int
     {
-        return 0;
+        return $number1+$number2;
     }
 
     function multiply(int $number1, int $number2): int
     {
-        return 0;
+        return $number1*$number2;
     }
 }
