019a513dc50d830f913cb84191692106416ad31d
Oihana Elizagoyen Ugalde (portátil) <elizagoyen.131910@e.unavarra.es>
2022-02-22 00:02:12 +0100

    Se ha realizado el ejercicio de Prime Factors Kata en el fichero PrimeFactors.php

--- src/PrimeFactors.php
+++ src/PrimeFactors.php
@@ -5,4 +5,30 @@ namespace Deg540\PHPTestingBoilerplate;
 class PrimeFactors
 {
 
+    public function calculatePrimeFactors (int $numberToFactor): array
+    {
+        $actualNumber = $numberToFactor;
+
+        while($actualNumber !== 1){
+            $newPrimeFactor = $this->calculatePrimeFactors($actualNumber);
+            $actualNumber = $actualNumber/$newPrimeFactor;
+            $result[] = $newPrimeFactor;
+        }
+
+        return ($result);
+
+    }
+
+    private function calculateFirstPrimeFactor(int $number): int
+    {
+        $rest = -1;
+        $possiblePrimeFactor = 1;
+
+        while($rest !== 0){
+            $possiblePrimeFactor++;
+            $rest = $number%$possiblePrimeFactor;
+        }
+
+        return ($possiblePrimeFactor);
+    }
 }
