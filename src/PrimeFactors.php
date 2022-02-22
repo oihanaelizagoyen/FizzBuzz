a1d20c30ddb0515ca64aa55f6e92d1b5655835eb
Oihana Elizagoyen Ugalde (portátil) <elizagoyen.131910@e.unavarra.es>
2022-02-22 08:39:18 +0100

    Listo para realizar el primer test para el ejercicio de Primer Factors Kata

--- src/PrimeFactors.php
+++ src/PrimeFactors.php
@@ -4,6 +4,15 @@ namespace Deg540\PHPTestingBoilerplate;
 
 class PrimeFactors
 {
+    public function calculatePrimeFactors(int $numberToFactor): array
+    {
+        $result = array();
+        $actualNumber = $numberToFactor;
 
+        while($actualNumber > 1){
 
+        }
+    }
+
+    public function 
 }
--- tests/PrimeFactorsTest.php
+++ tests/PrimeFactorsTest.php
@@ -7,26 +7,5 @@ use PHPUnit\Framework\TestCase;
 
 class PrimeFactorsTest extends TestCase
 {
-    private PrimeFactors $primeFactors;
-
-    /**
-     * @setUp
-     */
-    protected function setUp(): void
-    {
-        parent::setUp();
-        $this->primeFactors = new PrimeFactors();
-    }
-
-    /**
-     * @test
-     */
-    public function given_15_returns_3_5()
-    {
-        $expected = array(3, 5);
-
-        $result = $this->primeFactors->calculatePrimeFactors(15);
-
-        $this->assertEquals($expected, $result);
-    }
+    
 }
