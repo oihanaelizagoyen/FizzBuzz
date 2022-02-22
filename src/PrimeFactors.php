b5d96b73437c0476715976d2504273d60e749b06
Oihana Elizagoyen Ugalde (portátil) <elizagoyen.131910@e.unavarra.es>
2022-02-22 08:32:38 +0100

    Reiniciando el ejercicio Prime Factors Kata para realizarlo aplicando TDD

--- src/PrimeFactors.php
+++ src/PrimeFactors.php
@@ -5,31 +5,5 @@ namespace Deg540\PHPTestingBoilerplate;
 class PrimeFactors
 {
 
-    public function calculatePrimeFactors (int $numberToFactor): array
-    {
-        $result = array();
-        $actualNumber = $numberToFactor;
 
-        while($actualNumber !== 1){
-            $newPrimeFactor = $this->calculateFirstPrimeFactor($actualNumber);
-            $actualNumber = $actualNumber/$newPrimeFactor;
-            $result[] = $newPrimeFactor;
-        }
-
-        return ($result);
-
-    }
-
-    private function calculateFirstPrimeFactor(int $number): int
-    {
-        $rest = -1;
-        $possiblePrimeFactor = 1;
-
-        while($rest !== 0){
-            $possiblePrimeFactor++;
-            $rest = $number%$possiblePrimeFactor;
-        }
-
-        return ($possiblePrimeFactor);
-    }
 }
